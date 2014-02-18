655e800d529d64ce34b91ea0e02a7b48b7ad491e
jmorillofe <morillojuan@yahoo.es>
2014-02-18 22:31:01 +0100

    gfgfgf

--- ProyectoGIT/src/Hola.java
+++ ProyectoGIT/src/Hola.java
@@ -1,10 +1,10 @@
-
 public class Hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Primeros pasos de GIT
 		System.out.println("jjjjjj");
+		//holaaaa
 	}
 
 }
